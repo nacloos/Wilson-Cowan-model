abfb6fe30e1240084494db444c3071df7b5f645b
Nathan Cloos <cloosnathan@gmail.com>
2020-12-30 16:00:00 +0100

    Modif homoclinic plot.

--- WC_model/WC_plots.jl
+++ WC_model/WC_plots.jl
@@ -134,6 +134,85 @@ function plot_hysteresis(p::WCModel)
 end
 
 
+function plot_homoclinic(p::WCModel)
+
+    min_val = -1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ))
+    max_val = 1-1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ))
+    E_bounds = [0.1, 0.35]
+    I_bounds = [0.85, max_val]
+
+    I_ext = 9
+    # E_ext_values = [12.1, 12.2, 12.252, 12.42]
+    E_ext_values = [12.1, 12.190282435, 12.21, 12.28, 12.42]
+
+    figure(figsize=(12,3), dpi=130)
+    for (i, E_ext) in enumerate(E_ext_values)
+        subplot(1, length(E_ext_values), i)
+        global p = WCModel(p.E_pop, p.I_pop, p.wEE, p.wIE, p.wEI, p.wII, E_ext, I_ext)
+
+        # plot nullclines
+        E_nullcline, I_nullcline = nullclines(p)
+        values = range(-1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ)), stop=1-1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ)), length=1000)
+        I_itp = LinearInterpolation((I_nullcline(values),), values, extrapolation_bc=Flat())
+        x = range(values[1]+1e-8, stop=values[end]-1e-8, length=1000)
+        plot(x, E_nullcline(x), label="\$\\frac{dE}{dt}=0\$", color="dimgray", alpha=0.5, linewidth=1)
+        plot(x, I_itp(x), label="\$\\frac{dI}{dt}=0\$", color="dimgray", linestyle="--", alpha=0.5,  linewidth=1)
+
+        # plot trajectories
+        equilibria = find_equilibria(p, (min_val, max_val))
+        if size(equilibria)[1] == 3
+            if i >= 3
+                node = equilibria[1]
+                init_point = if (i==3) node.+3e-2 else node.+7.072598901e-3 end
+                sol = simulate(p, init_point, 50)
+                plot(sol[1,:], sol[2,:], color="mediumaquamarine", linewidth=1.5)
+                scatter(init_point..., marker=".", color="mediumaquamarine")
+            end
+            sn = equilibria[2]
+            J = jacobian(p, sn[1], sn[2], E_ext, I_ext)
+            J_eigvecs = eigvecs(J)
+
+            init_point = sn - 3.1735e-2 .*(J_eigvecs[:,1] .+ J_eigvecs[:,2])
+            sol = simulate(p, init_point, 20)
+            plot(sol[1,:], sol[2,:], color="coral", linewidth=1.5)
+            scatter(init_point..., marker=".", color="coral")
+
+
+
+            # init_point = sn + 5e-3 .*(J_eigvecs[:,1] .- J_eigvecs[:,2])
+            init_point = sn - 5e-4 .* J_eigvecs[:,2]
+            sol = simulate(p, init_point, 10)
+            plot(sol[1,:], sol[2,:],color="#4A4A48", linewidth=1.5)
+            scatter(init_point..., marker=".", color="#4A4A48")
+        else
+            init_point = [0.24, 0.95]
+            sol = simulate(p, init_point, 50)
+            plot(sol[1,:], sol[2,:], color="mediumaquamarine", linewidth=1.5)
+            scatter(init_point..., marker=".", color="mediumaquamarine")
+
+            init_point = [0.225, 0.95]
+            sol = simulate(p, init_point, 20)
+            plot(sol[1,:], sol[2,:],color="coral", linewidth=1.5)
+            scatter(init_point..., marker=".", color="coral")
+
+            init_point = [0.24, 0.97]
+            sol = simulate(p, init_point, 10)
+            plot(sol[1,:], sol[2,:], color="#4A4A48", linewidth=1.5)
+            scatter(init_point..., marker=".", color="#4A4A48")
+
+        end
+        if i != 2
+            title("\$E_{ext}=$E_ext\$")
+        end
+        axis("off")
+        ylim(0.84, max_val)
+        xlim(0.092, 0.333)
+    end
+    subplots_adjust(left=0.01, right=0.98, wspace=0)
+    savefig(savefig_path*"homoclinic.pdf")
+    savefig(savefig_path*"homoclinic.png", transparent=true)
+end
+
 
 tau_E=1.; a_E=1.5; theta_E=3.0
 tau_I=1.; a_I=1.5; theta_I=3.0
@@ -147,81 +226,4 @@ global p = WCModel(E_pop, I_pop, wEE, wIE, wEI, wII, E_ext, I_ext)
 
 # plot_limit_cycle(p)
 # plot_hysteresis(p)
-
-
-
-min_val = -1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ))
-max_val = 1-1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ))
-E_bounds = [0.1, 0.35]
-I_bounds = [0.85, max_val]
-
-I_ext = 9
-# E_ext_values = [12.1, 12.2, 12.252, 12.42]
-E_ext_values = [12.1, 12.190282435, 12.21, 12.28, 12.42]
-
-figure(figsize=(12,3), dpi=130)
-for (i, E_ext) in enumerate(E_ext_values)
-    subplot(1, length(E_ext_values), i)
-    global p = WCModel(p.E_pop, p.I_pop, p.wEE, p.wIE, p.wEI, p.wII, E_ext, I_ext)
-
-    equilibria = find_equilibria(p, (min_val, max_val))
-    if size(equilibria)[1] == 3
-        if i >= 3
-            node = equilibria[1]
-            init_point = if (i==3) node.+3e-2 else node.+7.072598901e-3 end
-            sol = simulate(p, init_point, 50)
-            plot(sol[1,:], sol[2,:], alpha=1, color="mediumaquamarine", linewidth=1.5)
-            scatter(init_point..., marker=".", alpha=1, color="mediumaquamarine")
-        end
-        sn = equilibria[2]
-        J = jacobian(p, sn[1], sn[2], E_ext, I_ext)
-        J_eigvecs = eigvecs(J)
-
-        init_point = sn - 3.1735e-2 .*(J_eigvecs[:,1] .+ J_eigvecs[:,2])
-        sol = simulate(p, init_point, 20)
-        plot(sol[1,:], sol[2,:], alpha=1, color="coral", linewidth=1.5)
-        scatter(init_point..., marker=".", alpha=1, color="coral")
-
-
-
-        # init_point = sn + 5e-3 .*(J_eigvecs[:,1] .- J_eigvecs[:,2])
-        init_point = sn - 5e-4 .* J_eigvecs[:,2]
-        sol = simulate(p, init_point, 10)
-        plot(sol[1,:], sol[2,:], alpha=1, color="#4A4A48", linewidth=1.5)
-        scatter(init_point..., marker=".", alpha=1, color="#4A4A48")
-    else
-        init_point = [0.24, 0.95]
-        sol = simulate(p, init_point, 50)
-        plot(sol[1,:], sol[2,:], alpha=1, color="mediumaquamarine", linewidth=1.5)
-        scatter(init_point..., marker=".", alpha=1, color="mediumaquamarine")
-
-        init_point = [0.225, 0.95]
-        sol = simulate(p, init_point, 20)
-        plot(sol[1,:], sol[2,:], alpha=1, color="coral", linewidth=1.5)
-        scatter(init_point..., marker=".", alpha=1, color="coral")
-
-        init_point = [0.24, 0.97]
-        sol = simulate(p, init_point, 10)
-        plot(sol[1,:], sol[2,:], alpha=1, color="#4A4A48", linewidth=1.5)
-        scatter(init_point..., marker=".", alpha=1, color="#4A4A48")
-
-    end
-    if i != 2
-        title("\$E_{ext}=$E_ext\$")
-    end
-    axis("off")
-    ylim(0.84, max_val)
-    xlim(0.092, 0.333)
-
-
-    E_nullcline, I_nullcline = nullclines(p)
-    values = range(-1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ)), stop=1-1/(1+exp(p.I_pop.act.a*p.I_pop.act.θ)), length=1000)
-    I_itp = LinearInterpolation((I_nullcline(values),), values, extrapolation_bc=Flat())
-    x = range(values[1]+1e-8, stop=values[end]-1e-8, length=1000)
-    plot(x, E_nullcline(x), label="\$\\frac{dE}{dt}=0\$", color="dimgray", alpha=0.5, linewidth=1)
-    plot(x, I_itp(x), label="\$\\frac{dI}{dt}=0\$", color="dimgray", linestyle="--", alpha=0.5,  linewidth=1)
-
-end
-subplots_adjust(left=0.01, right=0.98, wspace=0)
-savefig(savefig_path*"homoclinic.pdf")
-savefig(savefig_path*"homoclinic.png", transparent=true)
+plot_homoclinic(p)
